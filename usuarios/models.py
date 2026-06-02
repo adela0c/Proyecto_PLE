4453db57d41aeb1e1b0c89f90b1a97c0234bea7f
Adela Olivos <adelaolivos2@gmail.com>
2026-06-02 12:07:49 -0600

    Cambios en alumnos y models

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Alumno (models.Model):
+    Curso=[
+        ('Ingles', 'Frances')
+    ]
+    Nivel=[
+        ('Basico', 'Intermedio', 'Avanzado', 'Certificacion')
+    ]
+    nombre = models.CharField(max_length=100)
+    apellido=models.CharField(max_length=100)
+    edad=models.IntegerField
+    sexo=models.CharField(max_length=100)
+    categoria=models.CharField(max_length=100)
+
+class Profesores (models.Model):
+    nombre=models.CharField(max_length=100)
+    apellido=models.CharField(max_length=100)
+    curso=models.CharField(max_length=100)
+    
+
